0d6eaa7b755b7800154a446802d60aae044126cf
Ross Fletcher <rtbfletch@outlook.com>
2018-03-18 15:55:05 -0700

    initial commit with working Dialog Fragment

---
+++ app/src/main/java/com/epicodus/fragments/MainActivity.java
@@ -0,0 +1,44 @@
+package com.epicodus.fragments;
+
+import android.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.bMoodButton)
+    Button moodButton;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        moodButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fm = getFragmentManager();
+                MoodDialogFragment moodDialogFragment = new MoodDialogFragment();
+                moodDialogFragment.show(fm, "Sample Fragment");
+            }
+
+        });
+
+
+
+    }
+
+
+
+
+
+}
